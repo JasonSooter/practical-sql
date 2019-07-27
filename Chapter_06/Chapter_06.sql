7dcc8481624fccb2a9391d59067220d55d0e65b7
Jason Sooter <jason.sooter@gmail.com>
2019-07-27 16:06:41 -0500

    chore(chapter6): complete chapter 6

--- Chapter_06/Chapter_06.sql
+++ Chapter_06/Chapter_06.sql
@@ -188,6 +188,16 @@ WHERE
   schools_left.id IS NULL;
 
 --
+-- Why you need table name when querying multiple tables via Join
+-- => column reference "id" is ambiguous
+
+SELECT
+  id
+FROM
+  schools_left
+  LEFT JOIN schools_right ON schools_left.id = schools_right.id;
+
+--
 -- Listing 6-10: Querying specific columns in a join
 --
 
@@ -211,7 +221,9 @@ FROM
   schools_left AS lt
   LEFT JOIN schools_right AS rt ON lt.id = rt.id;
 
+--
 -- Listing 6-12: Joining multiple tables
+--
 
 CREATE TABLE schools_enrollment (
   id integer,
@@ -253,6 +265,7 @@ FROM
   LEFT JOIN schools_enrollment AS en ON lt.id = en.id
   LEFT JOIN schools_grades AS gr ON lt.id = gr.id;
 
+--
 -- Listing 6-13: Performing math on joined Census tables
 -- Decennial Census 2000. Full data dictionary at https://www.census.gov/prod/cen2000/doc/pl94-171.pdf
 -- Note: Some non-number columns have been given more descriptive names
@@ -309,10 +322,14 @@ CREATE TABLE us_counties_2000 (
 
 COPY us_counties_2000
 FROM
-  'C:\YourDirectory\us_counties_2000.csv' WITH (
-    FORMAT CSV,
-    HEADER
+  '/Users/jasonsooter/dev-space/practical-sql/Chapter_06/us_counties_2000.csv' WITH (
+    format csv,
+    header
 );
+
+--
+--
+--
 
 SELECT
   c2010.geo_name,
@@ -329,3 +346,115 @@ FROM
   ORDER BY
     pct_change DESC;
 
+--
+-- Exercises
+--
+--
+-- Find the difference in number of counties from 2000 to 2010
+--
+
+SELECT
+  (
+    SELECT
+      count(geo_name)
+    FROM
+      us_counties_2010) - (
+    SELECT
+      count(geo_name)
+    FROM
+      us_counties_2000) AS county_difference;
+
+--
+-- Exercise 1
+-- Determine missing counties in 2010 that were in 2000
+--
+
+SELECT
+  c2010.state_us_abbreviation AS c2010_state_us_abbreviation,
+  -- c2010.state_fips AS c2010_state_fips,
+  -- c2010.county_fips AS c2010_county_fips,
+  c2010.geo_name AS c2010_geo_name,
+  c2000.state_us_abbreviation AS c2000_state_us_abbreviation,
+  -- c2000.state_fips AS c2000_state_fips,
+  -- c2000.county_fips AS c2000_county_fips,
+  c2000.geo_name AS c2000_geo_name
+FROM
+  us_counties_2010 AS c2010
+  FULL JOIN us_counties_2000 AS c2000 ON c2010.state_fips = c2000.state_fips
+    AND c2010.county_fips = c2000.county_fips
+WHERE
+  c2010.county_fips IS NULL
+  OR c2000.county_fips IS NULL;
+
+-- 2000 census started at 3141 counties
+-- -1 to 3140 Clifton Forge city 51-560 was an independent city IN 2000.
+-- Now part of Alleghany county in 2010
+--
+-- +1 to 3141 Wrangell-Petersburg Census area split into 2 census areas
+--
+-- +1 to 3142 Skagway-Hoonah-Angoon Census Area, Alaska split into
+-- Skagway Municipality and Hoonah-Angoon Census Area
+--
+-- +1 to 3143 Wrangell-Petersburg Census Area split into
+--  Wrangell City and Petersburg cenus area
+--
+-- Exercise 2
+--
+-- Using either the median() or percentile_cont() functions in
+-- Chapter5 determine the median of the percent change in county population
+
+SELECT
+  min(percent_change),
+  percentile_cont(.25)
+  WITHIN GROUP (ORDER BY percent_change) "25th quartile",
+  percentile_cont(.5)
+  WITHIN GROUP (ORDER BY percent_change) median,
+  percentile_cont(.75)
+  WITHIN GROUP (ORDER BY percent_change) "75th quartile",
+  percentile_cont(.9)
+  WITHIN GROUP (ORDER BY percent_change) "90th quartile",
+  max(percent_change)
+FROM (
+  -- subquery to get difference and percent change (percent_change)
+  SELECT
+    round((CAST(c2010.p0010001 AS numeric(8, 1)) - c2000.p0010001) / c2000.p0010001 * 100, 2) "percent_change"
+  FROM
+    us_counties_2010 c2010
+    INNER JOIN us_counties_2000 c2000 ON c2010.state_fips = c2000.state_fips
+      AND c2010.county_fips = c2000.county_fips
+      AND c2010.p0010001 <> c2000.p0010001
+    ORDER BY
+      percent_change ASC) t;
+
+-- min -46.60%
+-- 25th quartile-2.5175%
+-- median 3.195%
+-- 75th quartile 10.2325%
+-- 90th quartile 20.62%
+-- max 110.35%
+--
+--
+-- Exercise 3
+--
+-- Which county had the greatest percentage loss of population between 2000 and 2010?
+-- Explain why?
+--
+
+SELECT
+  geo_name,
+  percent_change
+FROM (
+  -- subquery to geo_name and percent change
+  SELECT
+    c2010.geo_name,
+    round((CAST(c2010.p0010001 AS numeric(8, 1)) - c2000.p0010001) / c2000.p0010001 * 100, 2) AS percent_change
+  FROM
+    us_counties_2010 c2010
+    INNER JOIN us_counties_2000 c2000 ON c2010.state_fips = c2000.state_fips
+      AND c2010.county_fips = c2000.county_fips
+      AND c2010.p0010001 <> c2000.p0010001
+    ORDER BY
+      percent_change ASC) AS t
+LIMIT 1;
+
+-- St. Bernard Parish population declined by 46.60% due to Hurricane Katrina in 2005

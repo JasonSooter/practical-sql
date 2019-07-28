a5f3faefd53b73d8fc058004f421fe62eea59ad2
Jason Sooter <jason.sooter@gmail.com>
2019-07-27 23:59:23 -0500

    feat(chapter_7): finish Listing 7-8

--- Chapter_07/Chapter_07.sql
+++ Chapter_07/Chapter_07.sql
@@ -3,177 +3,285 @@
 -- by Anthony DeBarros
 -- Chapter 7 Code Examples
 --------------------------------------------------------------
+-- Surrounding identifiers with quotes (") allows for case sensitivity
+
+CREATE TABLE customers (
+  customer_id serial
+);
+
+CREATE TABLE Customers (
+  customer_id serial
+);
+
+-- => error `relation "customers" already exists`
+-- Use quotes around `Customers` if you want to make a second table
+-- with the same name but with first capital letter
+-- Keep in mind this is a bad idea
+
+CREATE TABLE "Customers" (
+  customer_id serial
+);
+
+-- => Success
+-- Keep in mind this is a bad idea
+-- This table will require quoting in future references such as:
+
+SELECT
+  *
+FROM
+  "Customers";
+
+DROP TABLE "Customers";
+
+DROP TABLE "customers";
+
+--
 -- Listing 7-1: Declaring a single-column natural key as primary key
 -- As a column constraint
+
 CREATE TABLE natural_key_example (
-  license_id varchar(10) CONSTRAINT license_key PRIMARY KEY,
+  license_id varchar(10) PRIMARY KEY,
   first_name varchar(50),
   last_name varchar(50)
 );
+
 -- Drop the table before trying again
+
 DROP TABLE natural_key_example;
+
 -- As a table constraint
+
 CREATE TABLE natural_key_example (
   license_id varchar(10),
   first_name varchar(50),
   last_name varchar(50),
   CONSTRAINT license_key PRIMARY KEY (license_id)
 );
+
 -- Listing 7-2: Example of a primary key violation
-INSERT INTO
-  natural_key_example (license_id, first_name, last_name)
-VALUES
-  ('T229901', 'Lynn', 'Malero');
-INSERT INTO
-  natural_key_example (license_id, first_name, last_name)
-VALUES
-  ('T229901', 'Sam', 'Tracy');
+
+INSERT INTO natural_key_example (license_id,
+  first_name,
+  last_name)
+    VALUES ('T229901',
+      'Lynn',
+      'Malero');
+INSERT INTO natural_key_example (license_id,
+  first_name,
+  last_name)
+    VALUES ('T229901',
+      'Sam',
+      'Tracy');
+-- => ERROR:  duplicate key value violates unique constraint "license_key"
+-- => DETAIL:  Key (license_id)=(T229901) already exists.
+-- fails because license_id is not unique
+--
+--
 -- Listing 7-3: Declaring a composite primary key as a natural key
-  CREATE TABLE natural_key_composite_example (
-    student_id varchar(10),
-    school_day date,
-    present boolean,
-    CONSTRAINT student_key PRIMARY KEY (student_id, school_day)
-  );
+
+CREATE TABLE natural_key_composite_example (
+  student_id varchar(10),
+  school_day date,
+  present boolean,
+  CONSTRAINT student_key PRIMARY KEY (student_id, school_day)
+);
+
 -- Listing 7-4: Example of a composite primary key violation
-INSERT INTO
-  natural_key_composite_example (student_id, school_day, present)
-VALUES(775, '1/22/2017', 'Y');
-INSERT INTO
-  natural_key_composite_example (student_id, school_day, present)
-VALUES(775, '1/23/2017', 'Y');
-INSERT INTO
-  natural_key_composite_example (student_id, school_day, present)
-VALUES(775, '1/23/2017', 'N');
+
+INSERT INTO natural_key_composite_example (student_id,
+  school_day,
+  present)
+    VALUES (775,
+      '1/22/2017',
+      'Y');
+INSERT INTO natural_key_composite_example (student_id,
+  school_day,
+  present)
+    VALUES (775,
+      '1/23/2017',
+      'Y');
+INSERT INTO natural_key_composite_example (student_id,
+  school_day,
+  present)
+    VALUES (775,
+      '1/23/2017',
+      'N');
+-- => ERROR:  duplicate key value violates unique constraint "student_key"
+-- => DETAIL:  Key (student_id, school_day)=(775, 2017-01-23) already exists.
+-- Error because the combination of student_id and school_day are not unique
+--
 -- Listing 7-5: Declaring a bigserial column as a surrogate key
-  CREATE TABLE surrogate_key_example (
-    order_number bigserial,
-    product_name varchar(50),
-    order_date date,
-    CONSTRAINT order_key PRIMARY KEY (order_number)
-  );
-INSERT INTO
-  surrogate_key_example (product_name, order_date)
-VALUES
-  ('Beachball Polish', '2015-03-17'),
-  ('Wrinkle De-Atomizer', '2017-05-22'),
-  ('Flux Capacitor', '1985-10-26');
+
+CREATE TABLE surrogate_key_example (
+  order_number bigserial,
+  product_name varchar(50),
+  order_date date,
+  CONSTRAINT order_key PRIMARY KEY (order_number)
+);
+
+INSERT INTO surrogate_key_example (product_name,
+  order_date)
+    VALUES ('Beachball Polish',
+      '2015-03-17'),
+    ('Wrinkle De-Atomizer',
+      '2017-05-22'),
+    ('Flux Capacitor',
+      '1985-10-26');
+--
+
 SELECT
   *
 FROM
   surrogate_key_example;
+
 -- Listing 7-6: A foreign key example
-  CREATE TABLE licenses (
-    license_id varchar(10),
-    first_name varchar(50),
-    last_name varchar(50),
-    CONSTRAINT licenses_key PRIMARY KEY (license_id)
-  );
+
+CREATE TABLE licenses (
+  license_id varchar(10),
+  first_name varchar(50),
+  last_name varchar(50),
+  CONSTRAINT licenses_key PRIMARY KEY (license_id)
+);
+
 CREATE TABLE registrations (
-    registration_id varchar(10),
-    registration_date date,
-    license_id varchar(10) REFERENCES licenses (license_id),
-    CONSTRAINT registration_key PRIMARY KEY (registration_id, license_id)
-  );
-INSERT INTO
-  licenses (license_id, first_name, last_name)
-VALUES
-  ('T229901', 'Lynn', 'Malero');
-INSERT INTO
-  registrations (registration_id, registration_date, license_id)
-VALUES
-  ('A203391', '3/17/2017', 'T229901');
-INSERT INTO
-  registrations (registration_id, registration_date, license_id)
-VALUES
-  ('A75772', '3/17/2017', 'T000001');
+  registration_id varchar(10),
+  registration_date date,
+  license_id varchar(10) REFERENCES licenses (license_id) ON DELETE CASCADE,
+  CONSTRAINT registration_key PRIMARY KEY (registration_id, license_id)
+);
+
+INSERT INTO licenses (license_id,
+  first_name,
+  last_name)
+    VALUES ('T229901',
+      'Lynn',
+      'Malero');
+INSERT INTO registrations (registration_id,
+  registration_date,
+  license_id)
+    VALUES ('A203391',
+      '3/17/2017',
+      'T229901');
+INSERT INTO registrations (registration_id,
+  registration_date,
+  license_id)
+    VALUES ('A75772',
+      '3/17/2017',
+      'T000001');
+-- ==> ERROR:  insert or update on table "registrations" violates foreign key constraint "registrations_license_id_fkey"
+-- ==> DETAIL:  Key (license_id)=(T000001) is not present in table "licenses".
+--
+--
 -- Listing 7-7: CHECK constraint examples
-  CREATE TABLE check_constraint_example (
-    user_id bigserial,
-    user_role varchar(50),
-    salary integer,
-    CONSTRAINT user_id_key PRIMARY KEY (user_id),
-    CONSTRAINT check_role_in_list CHECK (user_role IN('Admin', 'Staff')),
-    CONSTRAINT check_salary_not_zero CHECK (salary > 0)
-  );
+--
+
+CREATE TABLE check_constraint_example (
+  user_id bigserial,
+  user_role varchar(50),
+  salary integer,
+  CONSTRAINT user_id_key PRIMARY KEY (user_id),
+  CONSTRAINT check_role_in_list CHECK (user_role IN ('Admin',
+      'Staff')),
+  CONSTRAINT check_salary_not_zero CHECK (salary > 0)
+);
+
 -- Both of these will fail:
-INSERT INTO
-  check_constraint_example (user_role)
-VALUES
-  ('admin');
-INSERT INTO
-  check_constraint_example (salary)
-VALUES
-  (0);
+
+INSERT INTO check_constraint_example (user_role)
+    VALUES ('admin');
+INSERT INTO check_constraint_example (salary)
+    VALUES (0);
+--
 -- Listing 7-8: UNIQUE constraint example
-  CREATE TABLE unique_constraint_example (
-    contact_id bigserial CONSTRAINT contact_id_key PRIMARY KEY,
-    first_name varchar(50),
-    last_name varchar(50),
-    email varchar(200),
-    CONSTRAINT email_unique UNIQUE (email)
-  );
-INSERT INTO
-  unique_constraint_example (first_name, last_name, email)
-VALUES
-  ('Samantha', 'Lee', 'slee@example.org');
-INSERT INTO
-  unique_constraint_example (first_name, last_name, email)
-VALUES
-  ('Betty', 'Diaz', 'bdiaz@example.org');
-INSERT INTO
-  unique_constraint_example (first_name, last_name, email)
-VALUES
-  ('Sasha', 'Lee', 'slee@example.org');
+--
+
+CREATE TABLE unique_constraint_example (
+  contact_id bigserial CONSTRAINT contact_id_key PRIMARY KEY,
+  first_name varchar(50),
+  last_name varchar(50),
+  email varchar(200),
+  CONSTRAINT email_unique UNIQUE (email)
+);
+
+INSERT INTO unique_constraint_example (first_name,
+  last_name,
+  email)
+    VALUES ('Samantha',
+      'Lee',
+      'slee@example.org');
+INSERT INTO unique_constraint_example (first_name,
+  last_name,
+  email)
+    VALUES ('Betty',
+      'Diaz',
+      'bdiaz@example.org');
+INSERT INTO unique_constraint_example (first_name,
+  last_name,
+  email)
+    VALUES ('Sasha',
+      'Lee',
+      'slee@example.org');
+SELECT
+  *
+FROM
+  unique_constraint_example;
+
 -- Listing 7-9: NOT NULL constraint example
-  CREATE TABLE not_null_example (
-    student_id bigserial,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL,
-    CONSTRAINT student_id_key PRIMARY KEY (student_id)
-  );
+
+CREATE TABLE not_null_example (
+  student_id bigserial,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  CONSTRAINT student_id_key PRIMARY KEY (student_id)
+);
+
 -- Listing 7-10: Dropping and adding a primary key and a NOT NULL constraint
-  -- Drop
-ALTER TABLE
-  not_null_example DROP CONSTRAINT student_id_key;
--- Add
-ALTER TABLE
-  not_null_example
-ADD
-  CONSTRAINT student_id_key PRIMARY KEY (student_id);
 -- Drop
-ALTER TABLE
-  not_null_example
-ALTER COLUMN
-  first_name DROP NOT NULL;
+
+ALTER TABLE not_null_example DROP CONSTRAINT student_id_key;
+
 -- Add
-ALTER TABLE
-  not_null_example
-ALTER COLUMN
-  first_name
-SET
-  NOT NULL;
+
+ALTER TABLE not_null_example
+  ADD CONSTRAINT student_id_key PRIMARY KEY (student_id);
+
+-- Drop
+
+ALTER TABLE not_null_example ALTER COLUMN first_name DROP NOT NULL;
+
+-- Add
+
+ALTER TABLE not_null_example ALTER COLUMN first_name SET NOT NULL;
+
 -- Listing 7-11: Importing New York City address data
-  CREATE TABLE new_york_addresses (
-    longitude numeric(9, 6),
-    latitude numeric(9, 6),
-    street_number varchar(10),
-    street varchar(32),
-    unit varchar(7),
-    postcode varchar(5),
-    id integer CONSTRAINT new_york_key PRIMARY KEY
-  );
+
+CREATE TABLE new_york_addresses (
+  longitude numeric(9, 6),
+  latitude numeric(9, 6),
+  street_number varchar(10),
+  street varchar(32),
+  unit varchar(7),
+  postcode varchar(5),
+  id integer CONSTRAINT new_york_key PRIMARY KEY
+);
+
 COPY new_york_addresses
 FROM
-  'C:\YourDirectory\city_of_new_york.csv' WITH (FORMAT CSV, HEADER);
+  'C:\YourDirectory\city_of_new_york.csv' WITH (
+    FORMAT CSV,
+    HEADER
+);
+
 -- Listing 7-12: Benchmark queries for index performance
-  EXPLAIN ANALYZE
+
+EXPLAIN ANALYZE
 SELECT
   *
 FROM
   new_york_addresses
 WHERE
   street = 'BROADWAY';
+
 EXPLAIN ANALYZE
 SELECT
   *
@@ -181,6 +289,7 @@ FROM
   new_york_addresses
 WHERE
   street = '52 STREET';
+
 EXPLAIN ANALYZE
 SELECT
   *
@@ -188,5 +297,8 @@ FROM
   new_york_addresses
 WHERE
   street = 'ZWICKY AVENUE';
+
 -- Listing 7-13: Creating a B-Tree index on the new_york_addresses table
-  CREATE INDEX street_idx ON new_york_addresses (street);
+
+CREATE INDEX street_idx ON new_york_addresses (street);
+
